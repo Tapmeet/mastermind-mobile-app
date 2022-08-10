c0ec65137ca0250dee580f70beaa73aa73284dce
Tapmeet <tapmeet@gmail.com>
2022-08-10 14:46:40 +0530

    Push Notifications

--- App.js
+++ App.js
@@ -8,29 +8,98 @@ import configureStore from "./src/redux/store";
 import { View } from "react-native";
 import { Text } from "native-base";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-export default class App extends React.Component {
-  state = {
-    loading: true,
-  };
-  async componentDidMount() {
-    await Font.loadAsync({ 
-      HKGrotesk: require("./assets/fonts/HKGrotesk-Regular.ttf"),
-      Poppins: require("./assets/fonts/Poppins-Regular.ttf"),
-      PoppinsBold: require("./assets/fonts/Poppins-Medium.ttf"),
-      Roboto_medium: require("./assets/fonts/Roboto-Regular.ttf"),
-      ...Ionicons.font,
+import * as Notifications from 'expo-notifications';
+import { useFonts } from 'expo-font';
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+// export default class App extends React.Component {
+//   state = {
+//     loading: true,
+//   };
+//   async componentDidMount() {
+//     await Font.loadAsync({ 
+//       HKGrotesk: require("./assets/fonts/HKGrotesk-Regular.ttf"),
+//       Poppins: require("./assets/fonts/Poppins-Regular.ttf"),
+//       PoppinsBold: require("./assets/fonts/Poppins-Medium.ttf"),
+//       Roboto_medium: require("./assets/fonts/Roboto-Regular.ttf"),
+//       ...Ionicons.font,
+//     });
+//     this.setState({ loading: false });
+//   }
+//   render() {
+//     if (this.state.loading) {
+//       return <AppLoading />;
+//     } else {
+//       return (
+//         <StoreProvider store={configureStore()}>
+//           <Main />
+//         </StoreProvider>
+//       );
+//     }
+//   }
+// }
+
+export default function App() {
+  const [expoPushToken, setExpoPushToken] = React.useState('');
+  const [notification, setNotification] = React.useState(false);
+  const notificationListener = React.useRef();
+  const responseListener = React.useRef();
+  let [fontsLoaded] = useFonts({
+    HKGrotesk: require("./assets/fonts/HKGrotesk-Regular.ttf"),
+    Poppins: require("./assets/fonts/Poppins-Regular.ttf"),
+    PoppinsBold: require("./assets/fonts/Poppins-Medium.ttf"),
+    Roboto_medium: require("./assets/fonts/Roboto-Regular.ttf"),
+    ...Ionicons.font,
+  });
+
+  React.useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
     });
-    this.setState({ loading: false });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+ 
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
-  render() {
-    if (this.state.loading) {
-      return <AppLoading />;
-    } else {
-      return (
-        <StoreProvider store={configureStore()}>
-          <Main />
-        </StoreProvider>
-      );
+  else {
+    return (
+      <StoreProvider store={configureStore()}>
+        <Main />
+      </StoreProvider>
+    );
+  }
+  async function registerForPushNotificationsAsync() {
+    let token;
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
     }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+
+    return token;
   }
 }
